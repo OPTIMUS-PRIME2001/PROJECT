a814b726da8dd6ee5317f0ffc8cb3fbd52b710bd
PRIME_SAPTARSHI <chatterjeeu7@gmail.com>
2022-10-14 18:14:02 +0530

    PRIMUS

--- Create_DB.py
+++ Create_DB.py
@@ -1,5 +1,7 @@
 import sqlite3
 def create_DB():
     con=sqlite3.connect(database=r'InventoryData.db')
-
+    cur=con.cursor()
+    cur.execute("CREATE TABLE IF NOT EXISTS category(cid INTEGER PRIMARY KEY AUTOINCREMENT,name TEXT)")
+    con.commit()
 create_DB()
