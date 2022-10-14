b318ce3de4b288ddf4f0ee94660bad8cbe055a14
PRIME_SAPTARSHI <chatterjeeu7@gmail.com>
2022-10-14 14:52:03 +0530

    PRIMUS

--- Category.py
+++ Category.py
@@ -10,7 +10,8 @@ class categoryClass:
        self.root.config(bg="white") 
        self.root.focus_force()
        #========title========
-       lbl_title=Label(self.root,text="Manage Product Category",font=("goudy old style",30),bg="#184a45",fg="white").pack(side=TOP,fill=X)
+       lbl_title=Label(self.root,text="Manage Product Category",font=("goudy old style",30),bg="#184a45",fg="white",bd=3,relief=RIDGE).pack(side=TOP,fill=X,padx=10,pady=10)
+       lbl_name=Label(self.root,text="Enter Category",font=("goudy old style",30),bg="white").pack(side=TOP,fill=X,padx=10,pady=10)
 if __name__=="__main__":
     root=Tk()
     obj=categoryClass(root)
