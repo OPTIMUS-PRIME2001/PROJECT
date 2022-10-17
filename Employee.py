04c74be133566a4d7f65f34bd6612a00b4706072
PRIME_SAPTARSHI <chatterjeeu7@gmail.com>
2022-10-17 07:27:22 +0530

    PRIMUS

--- Employee.py
+++ Employee.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from PIL import Image, ImageTk
-from tkinter import ttk
+from tkinter import ttk,messagebox
+import sqlite3 
 
 class employeeClass:
     def __init__(self, root):
@@ -31,7 +32,7 @@ class employeeClass:
         SearchFrame=LabelFrame(self.root,text="Search Employee",font=("goudy old style",12,"bold"),bd=2,relief=RIDGE,bg="white")
         SearchFrame.place(x=250,y=20,width=600,height=70)
         
-        #===Options===
+        #===Options=====
         cmb_search=ttk.Combobox(SearchFrame,textvariable=self.var_searchby,values=("Select","Email","Name","Contact"),state='readonly',justify=CENTER,font=("goudy old style",15))
         cmb_search.place(x=10,y=10,width=180)
         cmb_search.current(0)
@@ -39,7 +40,7 @@ class employeeClass:
         txt_search=Entry(SearchFrame,textvariable=self.var_searchtxt,font=("goudy old style",15),bg="lightyellow").place(x=200,y=10)
         btn_search=Button(SearchFrame,text="Search",font=("goudy old style",15),bg="#4caf50",fg="white",cursor="hand2").place(x=410,y=10,width=150,height=30)
         
-        #==title==
+        #==title=====
         title=Label(self.root,text="Employee Details",font=("goudy old style",15),bg="#0f4d7d",fg="white").place(x=50,y=100,width=1000)
 
         # #=====Content======
@@ -89,12 +90,11 @@ class employeeClass:
         txt_salary=Entry(self.root,textvariable=self.var_salary,font=("goudy old style",15),bg="lightyellow").place(x=500,y=270,width=180)
         
         #=====Button=====
-        btn_add=Button(self.root,text="Save",font=("goudy old style",15),bg="#2196f3",fg="white",cursor="hand2").place(x=500,y=305,width=110,height=28)
-        btn_update=Button(self.root,text="Update",font=("goudy old style",15),bg="#4caf50",fg="white",cursor="hand2").place(x=620,y=305,width=110,height=28)
-        btn_delete=Button(self.root,text="Delete",font=("goudy old style",15),bg="#f44336",fg="white",cursor="hand2").place(x=740,y=305,width=110,height=28)
-        btn_clear=Button(self.root,text="Clear",font=("goudy old style",15),bg="#607d8b",fg="white",cursor="hand2").place(x=860,y=305,width=110,height=28)
+        btn_add=Button(self.root,text="Save",command=self.add,font=("goudy old style",15),bg="#2196f3",fg="white",cursor="hand2").place(x=500,y=305,width=110,height=28)
+        btn_update=Button(self.root,text="Update",command=self.update,font=("goudy old style",15),bg="#4caf50",fg="white",cursor="hand2").place(x=620,y=305,width=110,height=28)
+        btn_delete=Button(self.root,text="Delete",command=self.delete,font=("goudy old style",15),bg="#f44336",fg="white",cursor="hand2").place(x=740,y=305,width=110,height=28)
+        btn_clear=Button(self.root,text="Clear",command=self.clear,font=("goudy old style",15),bg="#607d8b",fg="white",cursor="hand2").place(x=860,y=305,width=110,height=28)
         
-
         #=======Employee Details======
 
         emp_frame=Frame(self.root,bd=3,relief=RIDGE)
@@ -123,7 +123,6 @@ class employeeClass:
         self.EmployeeTable.heading("address",text="Address")
         self.EmployeeTable.heading("salary",text="Salary")
 
-
         self.EmployeeTable["show"]="headings"
 
         self.EmployeeTable.column("eid",width=90)
@@ -137,9 +136,157 @@ class employeeClass:
         self.EmployeeTable.column("utype",width=100)
         self.EmployeeTable.column("address",width=100)
         self.EmployeeTable.column("salary",width=100)
-
-
         self.EmployeeTable.pack(fill=BOTH,expand=1)
+        self.EmployeeTable.bind("<ButtonRelease-1",self.get_data)
+
+        self.show()
+
+    #=================================
+
+    def add(self):
+        con = sqlite3.connect(database = r'InventoryData.db')
+        cur = con.cursor()
+        try:
+            if self.var_emp_id.get() == "" or self.var_name.get()=="":
+                messagebox.showerror("Error","Employee ID Must be required",parent=root)
+            else:
+                cur.execute("Select * from employee where eid=?",(self.var_emp_id.get(),))
+                row=cur.fetchone()
+                if row!=None:
+                    messagebox.showerror("Error","This Employee ID already assigned, try different",parent=self.root)
+                else:
+                    cur.execute("Insert into employee (eid,name,email,gender,contact,dob,doj,pass,utype,address,salary) values(?,?,?,?,?,?,?,?,?,?,?)",(
+                                self.var_emp_id.get(),
+                                self.var_name.get(),
+                                self.var_email.get(),
+                                self.var_gender.get(),
+                                self.var_contact.get(),
+                                self.var_dob.get(),
+                                self.var_doj.get(),                                
+                                self.var_pass.get(),
+                                self.var_utype.get(),
+                                self.txt_address.get('1.0',END),
+                                self.var_salary.get()
+                    ))
+                    con.commit()
+                    messagebox.showinfo("Success","Employee Addedd successfully",parent=self.root)
+                    self.show()        
+        except Exception as ex:
+            messagebox.showerror("Error",f"Error due to: {str(ex)}",parent=self.root)
+
+    def update(self):
+        con = sqlite3.connect(database = r'InventoryData.db')
+        cur = con.cursor()
+        try:
+            if self.var_emp_id.get() == "" or self.var_name.get()=="":
+                messagebox.showerror("Error","Employee ID Must be required",parent=root)
+            else:
+                cur.execute("Select * from employee where eid=?",(self.var_emp_id.get(),))
+                row=cur.fetchone()
+                if row==None:
+                    messagebox.showerror("Error","Invalid Employee ID",parent=self.root)
+                else:
+                    cur.execute("Update employee set name=?,email=?,gender=?,contact=?,dob=?,doj=?,pass=?,utype=?,address=?,salary=? WHERE eid=? ",(
+                                self.var_name.get(),
+                                self.var_email.get(),
+                                self.var_gender.get(),
+                                self.var_contact.get(),
+                                self.var_dob.get(),
+                                self.var_doj.get(),                                
+                                self.var_pass.get(),
+                                self.var_utype.get(),
+                                self.txt_address.get('1.0',END),
+                                self.var_salary.get(),
+                                self.var_emp_id.get(),
+                    ))
+                    con.commit()
+                    messagebox.showinfo("Success","Employee Updated successfully",parent=self.root)
+                    self.show()        
+        except Exception as ex:
+            messagebox.showerror("Error",f"Error due to: {str(ex)}",parent=self.root)
+
+    def show(self):
+        con = sqlite3.connect(database = r'InventoryData.db')
+        cur = con.cursor()
+        try:
+            cur.execute("select * from employee")
+            rows=cur.fetchall()
+            self.EmployeeTable.delete(*self.EmployeeTable.get_children())
+            for row in rows:
+                self.EmployeeTable.insert('',END,values=row)
+
+        except Exception as ex:
+            messagebox.showerror("Error",f"Error due to: {str(ex)}",parent=self.root)
+
+    def delete(self):
+        con = sqlite3.connect(database = r'InventoryData.db')
+        cur = con.cursor()
+        try:
+            if self.var_emp_id.get() == "" or self.var_name.get()=="":
+                messagebox.showerror("Error","Employee ID Must be required",parent=root)
+            else:
+                cur.execute("Select * from employee where eid=?",(self.var_emp_id.get(),))
+                row=cur.fetchone()
+                if row==None:
+                    messagebox.showerror("Error","Invalid Employee ID",parent=self.root)
+                else:
+                    op = messagebox.askyesno("Confirm","Do you want to delete?",parent=self.root)
+                    if op==True:
+                        cur.execute("delete from employee where eid=?", (self.var_emp_id.get(),))
+                        con.commit()
+                        messagebox.showinfo("Delete","Employee Deleted successfull",parent=self.root)
+                        self.clear()
+
+        except Exception as ex:
+            messagebox.showerror("Error",f"Error due to: {str(ex)}",parent=self.root)
+
+    def get_data(self):
+        f=self.EmployeeTable.focus()
+        content=(self.EmployeeTable.item(f))
+        row=content['values']
+        print(row)
+        self.var_emp_id.set(row[0]),
+        self.var_name.set(row[1]),
+        self.var_email.set(row[2]),
+        self.var_gender.set(row[3]),
+        self.var_contact.set(row[4]),
+        self.var_dob.set(row[5]),
+        self.var_doj.set(row[6]),                                
+        self.var_pass.set(row[7]),
+        self.var_utype.set(row[8]),
+        self.txt_address.delete('1.0',END),
+        self.txt_address.insert(END,row[9]),
+        self.var_salary.set(row[10])
+
+    def clear(self):
+        self.var_emp_id.set(""),
+        self.var_name.set(""),
+        self.var_email.set(""),
+        self.var_gender.set("Select"),
+        self.var_contact.set(""),
+        self.var_dob.set(""),
+        self.var_doj.set(""),                                
+        self.var_pass.set(""),
+        self.var_utype.set("Admin"),
+        self.txt_address.delete('1.0',END),
+        self.var_salary.set("")
+        self.show()
+
+    def search(self):
+        con = sqlite3.connect(database = r'InventoryData.db')
+        cur = con.cursor()
+        try:
+            if self.var_searchby.get()=="":
+                messagebox.showinfo()
+                cur.execute("select * from employee")
+            rows=cur.fetchall()
+            self.EmployeeTable.delete(*self.EmployeeTable.get_children())
+            for row in rows:
+                self.EmployeeTable.insert('',END,values=row)
+
+        except Exception as ex:
+            messagebox.showerror("Error",f"Error due to: {str(ex)}",parent=self.root)
+
 if __name__=="__main__":
     root=Tk()
     obj=employeeClass(root)
