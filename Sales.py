4db508669d29eb921b1243adcba820d3d04fb5f9
FireDrop6000 <pratik.neel02@gmail.com>
2022-10-21 00:50:13 +0530

    Pratik

--- Sales.py
+++ Sales.py
@@ -12,6 +12,7 @@ class salesClass:
        self.root.config(bg="white") 
        self.root.focus_force()
        
+       self.bill_list=[]
        self.var_invoice = StringVar()
        #title====================
        lbl_title=Label(self.root,text="View Customer Bills",font=("goudy old style",30),bg="#184a45",fg="white",bd=3,relief=RIDGE).pack(side=TOP,fill=X,padx=10,pady=10)
@@ -20,8 +21,8 @@ class salesClass:
 
        txt_invoice = Entry(self.root, textvariable = self.var_invoice, font = ("times new roman", 15), bg = "lightyellow").place(x = 160, y = 100, width = 180, height = 28)
 
-       btn_search = Button(self.root, text = "Search", font = ("times new roman", 15, "bold"), bg = "#2196f3", fg = "white", cursor = "hand2").place(x = 360, y = 100,  width = 120, height = 28)
-       btn_clear = Button(self.root, text = "Clear", font = ("times new roman", 15, "bold"), bg = "lightgray", cursor = "hand2").place(x = 490, y = 100,  width = 120, height = 28)
+       btn_search = Button(self.root, text = "Search", command=self.search,font = ("times new roman", 15, "bold"), bg = "#2196f3", fg = "white", cursor = "hand2").place(x = 360, y = 100,  width = 120, height = 28)
+       btn_clear = Button(self.root, text = "Clear", command=self.clear,font = ("times new roman", 15, "bold"), bg = "lightgray", cursor = "hand2").place(x = 490, y = 100,  width = 120, height = 28)
 
        #===Bill List===
        sales_Frame = Frame(self.root, bd=3,relief = RIDGE)
@@ -32,6 +33,7 @@ class salesClass:
        scrolly.pack(side=RIGHT,fill=Y)
        scrolly.config(command=self.Sales_List.yview)
        self.Sales_List.pack(fill=BOTH,expand=1)
+       self.Sales_List.bind("<ButtonRelease-1>",self.get_data)
 
        #===Bill Area===
        bill_Frame = Frame(self.root, bd=3,relief = RIDGE)
@@ -56,10 +58,43 @@ class salesClass:
        lbl_image=Label(self.root,image = self.bill_photo,bd=0)
        lbl_image.place(x=700,y=110)
 
+       self.show()
 #==================================================
-#    def show(self):
-
+    def show(self):
+        del self.bill_list[:]
+        self.Sales_List.delete(0, END)
+        # print(os.listdir('../PROJECT'))
+        for i in os.listdir('bill'):
+            if i.split('.')[-1]=='txt':
+                self.Sales_List.insert(END,i)
+                self.bill_list.append(i.split('.')[0])
         
+    def get_data(self,ev):
+        index_ = self.Sales_List.curselection()
+        file_name=self.Sales_List.get(index_)
+        # print(file_name)
+        self.bill_area.delete('1.0',END)
+        fp=open(f'bill/{file_name}','r')
+        for i in fp:
+            self.bill_area.insert(END,i)
+        fp.close()
+
+    def search(self):
+        if self.var_invoice.get()=="":
+            messagebox.showerror("Error", "Invoice No. should be required", parent=self.root)
+        else:
+            if self.var_invoice.get() in self.bill_list:
+                fp=open(f'bill/{self.var_invoice.get()}.txt','r')
+                self.bill_area.delete('1.0', END)
+                for i in fp:
+                    self.bill_area.insert(END,i)
+                fp.close()
+            else:
+                messagebox.showerror("Error", "Invalid Invoice No.", parent=self.root)
+
+    def clear(self):
+        self.show()
+        self.bill_area.delete("1.0",END)
 
 
 if __name__=="__main__":
